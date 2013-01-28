50e12cdd991961f79bd7454543a2fec2b4bd8ab2
Grzegorz Adam Hankiewicz <gradha@imap.cc>
2013-01-29 00:23:11 +0100

    Adds structures for input and output of parsing. Refs #4.

--- argument_parser.nim
+++ argument_parser.nim
@@ -1,6 +1,53 @@
-import os
+import os, strutils
 
-proc parse*(args: seq[TaintedString] = nil) =
+# - Types
+
+type
+  Tparameter_specification* = object
+    ## Holds the expectations of a parameter.
+    single_letter: char
+    single_word: string
+    double_word: string
+    consumes: bool
+
+  Tcommandline_results* = object
+    ## Contains the results of the parsing.
+    files: seq[string]
+
+
+# - Tparameter_specification procs
+
+proc init*(param: var Tparameter_specification, single_letter = ' ',
+    single_word = "", double_word = "", consumes = false) =
+  # Initialization helper.
+  param.single_letter = single_letter
+  param.single_word = single_word
+  param.double_word = double_word
+  param.consumes = consumes
+
+proc new_parameter_specification*(single_letter = ' ', single_word = "",
+    double_word = "", consumes = false): Tparameter_specification =
+  # Initialization helper for let variables.
+  result.init(single_letter, single_word, double_word, consumes)
+
+# - Tcommandline_results procs
+
+proc init*(param: var Tcommandline_results; files: seq[string] = @[]) =
+  # Initialization helper.
+  param.files = files
+
+proc new_commandline_results*(files: seq[string]): Tcommandline_results =
+  # Initialization helper for let variables.
+  result.init(files)
+
+proc `$`*(data: Tcommandline_results): string =
+  # Stringifies a Tcommandline_results structure for debug output
+  return "Tcommandline_result{files:$1}" % [join(
+    map(data.files) do (x: string) -> string: x.escape(), ", ")]
+
+proc parse*(expected: seq[Tparameter_specification],
+    args: seq[TaintedString] = nil): Tcommandline_results =
+  result.init()
   var args = args
   if args == nil:
     let total_params = ParamCount()
--- tester.nim
+++ tester.nim
@@ -2,7 +2,11 @@ import argument_parser, strutils
 
 when isMainModule:
   echo "Parsing default system params"
-  parse()
+  let p1 = new_parameter_specification(single_letter = 'a', consumes = true)
+  let p2 = new_parameter_specification(double_word = "aasd", consumes = true)
+  let ret1 = parse(@[p1, p2])
+  echo ($ret1)
   let args = @["test", "toca me la", "-a", "-wo", "rd", "--aasd", "--s"]
   echo "Parsing ", join(args, " ")
-  parse(args)
+  let ret2 = parse(@[p1, p2], args)
+  echo ($ret2)
