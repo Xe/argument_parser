85424cd1ed014fa8c6fd2ae4c9d782daec0fa4bc
Grzegorz Adam Hankiewicz <gradha@imap.cc>
2013-01-30 00:11:21 +0100

    Adds parsing of types for parameter values. Refs #5.

--- argument_parser.nim
+++ argument_parser.nim
@@ -1,38 +1,66 @@
-import os, strutils, tables, math
+import os, strutils, tables, math, parseutils
 
 # - Types
 
 type
+  ## Different types of results for parameter parsing.
+  Tparam_kind* = enum pkEmpty, pkInt, pkFloat, pkString, pkBool,
+    pkBiggestInt, pkBiggestFloat
+
   Tparameter_specification* = object
     ## Holds the expectations of a parameter.
     single_word: string
     double_word: string
-    consumes: bool
+    consumes: Tparam_kind
+
+  Tparsed_parameter* = object
+    case kind*: Tparam_kind
+    of pkEmpty: nil
+    of pkInt: intVal*: int
+    of pkBiggestInt: bigIntVal*: biggestInt
+    of pkFloat: floatVal*: float
+    of pkBiggestFloat: bigFloatVal: biggestFloat
+    of pkString: strVal*: string
+    of pkBool: boolVal*: bool
 
   Tcommandline_results* = object
     ## Contains the results of the parsing.
     files*: seq[string]
-    options*: TTable[string, string]
+    options*: TTable[string, Tparsed_parameter]
 
 
 # - Tparameter_specification procs
 
 proc init*(param: var Tparameter_specification,
-    single_word = "", double_word = "", consumes = false) =
+    single_word = "", double_word = "", consumes = pkEmpty) =
   # Initialization helper.
   param.single_word = single_word
   param.double_word = double_word
   param.consumes = consumes
 
 proc new_parameter_specification*(single_word = "",
-    double_word = "", consumes = false): Tparameter_specification =
+    double_word = "", consumes = pkEmpty): Tparameter_specification =
   # Initialization helper for let variables.
   result.init(single_word, double_word, consumes)
+
+# - Tparsed_parameter procs
+
+proc `$`*(data: Tparsed_parameter): string =
+  # Stringifies the value.
+  case data.kind:
+  of pkEmpty: result = "nil"
+  of pkInt: result = $data.intVal
+  of pkBiggestInt: result = $data.bigIntVal
+  of pkFloat: result = $data.floatVal
+  of pkBiggestFloat: result = $data.bigFloatVal
+  of pkString: result = $data.strVal
+  of pkBool: result = $data.boolVal
 
 # - Tcommandline_results procs
 
 proc init*(param: var Tcommandline_results; files: seq[string] = @[];
-    options: TTable[string, string] = initTable[string, string](4)) =
+    options: TTable[string, Tparsed_parameter] =
+      initTable[string, Tparsed_parameter](4)) =
   # Initialization helper.
   param.files = files
   param.options = options
@@ -41,15 +69,73 @@ proc `$`*(data: Tcommandline_results): string =
   # Stringifies a Tcommandline_results structure for debug output
   var dict: seq[string] = @[]
   for key, value in data.options:
-    dict.add("$1: $2" % [key.escape(), value.escape()])
+    dict.add("$1: $2" % [escape(key), escape($value)])
   result = "Tcommandline_result{files:[$1], options:{$2}}" % [join(
     map(data.files) do (x: string) -> string: x.escape(), ", "),
     join(dict, ", ")]
 
 # - Parse code
 
+proc parse_parameter(param, value: string,
+    param_kind: Tparam_kind): Tparsed_parameter =
+  ## Tries to parse a text according to the specified type.
+  ##
+  ## Pass the parameter string which requires a value and the text the user
+  ## passed in for it. It will be parsed according to the param_kind. This proc
+  ## will raise (EInvalidValue, EOverflow) if something can't be parsed.
+  result.kind = param_kind
+  case param_kind:
+  of pkInt:
+    try: result.intVal = value.parseInt
+    except EOverflow:
+      raise newException(EOverflow, ("parameter $1 requires an " &
+        "integer, but $2 is too large to fit into one") % [escape(param),
+        escape(value)])
+    except EInvalidValue:
+      raise newException(EInvalidValue, ("parameter $1 requires an " &
+        "integer, but $2 can't be parsed into one") % [escape(param),
+        escape(value)])
+  of pkString:
+    result.strVal = value
+  of pkFloat:
+    try: result.floatVal = value.parseFloat
+    except EInvalidValue:
+      raise newException(EInvalidValue, ("parameter $1 requires a " &
+        "float, but $2 can't be parsed into one") % [escape(param),
+        escape(value)])
+  of pkBool:
+    try: result.boolVal = value.parseBool
+    except EInvalidValue:
+      raise newException(EInvalidValue, ("parameter $1 requires a " &
+        "boolean, but $2 can't be parsed into one. Valid values are: " &
+        "y, yes, true, 1, on, n, no, false, 0, off") % [escape(param),
+        escape(value)])
+  of pkBiggestInt:
+    try:
+      if value.len != parseBiggestInt(value, result.bigIntVal):
+        raise newException(EInvalidValue, ("parameter $1 requires an " &
+          "integer, but $2 can't be parsed completely into one") % [
+          escape(param), escape(value)])
+    except EInvalidValue:
+      raise newException(EInvalidValue, ("parameter $1 requires an " &
+        "integer, but $2 can't be parsed into one") % [escape(param),
+        escape(value)])
+  of pkBiggestFloat:
+    try:
+      if value.len != parseBiggestFloat(value, result.bigFloatVal):
+        raise newException(EInvalidValue, ("parameter $1 requires a " &
+          "float, but $2 can't be parsed completely into one") % [
+          escape(param), escape(value)])
+    except EInvalidValue:
+      raise newException(EInvalidValue, ("parameter $1 requires a " &
+        "float, but $2 can't be parsed into one") % [escape(param),
+        escape(value)])
+  of pkEmpty:
+    nil
+
 proc parse*(expected: seq[Tparameter_specification],
     args: seq[TaintedString] = nil): Tcommandline_results =
+  var expected = expected
   result.init()
   # Prepare the input parameter list, maybe get it from the OS if not available.
   var args = args
@@ -62,7 +148,7 @@ proc parse*(expected: seq[Tparameter_specification],
       args[i] = paramStr(i + 1)
 
   # Generate lookup table for each type of parameter based on strings.
-  var lookup = initTable[string, Tparameter_specification](
+  var lookup = initTable[string, ptr Tparameter_specification](
     nextPowerOfTwo(expected.len))
   for i in 0..expected.len-1:
     let
@@ -73,12 +159,12 @@ proc parse*(expected: seq[Tparameter_specification],
       if lookup.hasKey(single_switch):
         quit("Parameter $1 repeated in input specification" % single_switch)
       else:
-        lookup[single_switch] = parameter_specification
+        lookup[single_switch] = addr(expected[i])
     if double_switch.len > 2:
       if lookup.hasKey(double_switch):
         quit("Parameter $1 repeated in input specification" % double_switch)
       else:
-        lookup[double_switch] = parameter_specification
+        lookup[double_switch] = addr(expected[i])
 
   # Loop through the input arguments detecting their type and doing stuff.
   var i = 0
@@ -87,16 +173,17 @@ proc parse*(expected: seq[Tparameter_specification],
     #echo "Arg ", $i, " value '", arg, "'"
     if arg.len > 0:
       if lookup.hasKey(arg):
+        var parsed : Tparsed_parameter
         let param = lookup[arg]
-        var next = ""
-        if param.consumes:
+        if param.consumes != pkEmpty:
           if i + 1 < args.len:
+            parsed = parse_parameter(arg, args[i + 1], param.consumes)
             i += 1
-            next = args[i]
           else:
-            quit("Parameter $1 requires value" % arg)
+            raise newException(EInvalidValue, ("parameter $1 requires a " &
+              "value, but none was provided") % [escape(arg)])
         #echo "\tFound ", arg, " ", next
-        result.options[arg] = next
+        result.options[arg] = parsed
       else:
         if arg[0] == '-':
           quit("Found unexpected parameter $1" % arg)
@@ -108,6 +195,6 @@ proc parse*(expected: seq[Tparameter_specification],
       result.files.add(arg)
     i += 1
 
+
 when isMainModule:
   echo "Welcome to argument_parser!"
-  parse()
--- tester.nim
+++ tester.nim
@@ -1,15 +1,74 @@
 import argument_parser, strutils, tables
 
-when isMainModule:
-  echo "Parsing default system params"
-  let p1 = new_parameter_specification(single_word = "a", consumes = true)
-  let p2 = new_parameter_specification(double_word = "aasd", consumes = true)
-  let ret1 = parse(@[p1, p2])
-  echo ($ret1)
+template test_failure(exception, code: expr): stmt =
+  try:
+    let pos = instantiationInfo()
+    discard(code)
+    echo "Unit test failure at $1:$2 with '$3'" % [pos.filename,
+      $pos.line, astToStr(code)]
+    assert false, "A test expecting failure succeeded?"
+  except exception:
+    nil
+
+template test_success(code: expr) {.immediate.} = echo($code)
+
+proc test() =
+  echo "\nParsing default system params"
+  let
+    p1 = new_parameter_specification(single_word = "a", consumes = pkString)
+    p2 = new_parameter_specification(double_word = "aasd", consumes = pkString)
+    p3 = new_parameter_specification(single_word = "i", consumes = pkInt)
+    p4 = new_parameter_specification(single_word = "f", consumes = pkFloat)
+    p5 = new_parameter_specification(single_word = "b", consumes = pkBool)
+    p6 = new_parameter_specification(single_word = "I", consumes = pkBiggestInt)
+    p7 = new_parameter_specification(single_word = "F",
+      consumes = pkBiggestFloat)
+    all_params = @[p1, p2, p3, p4, p5, p6, p7]
+
+  let ret1 = parse(all_params)
+  echo($ret1)
+
   let args = @["test", "toca me la", "-a", "-wo", "rd", "--aasd", "--s"]
-  echo "Parsing ", join(args, " ")
-  let ret2 = parse(@[p1, p2], args)
-  echo ($ret2)
-  assert ret2.options["-a"] == "-wo"
+  echo "\nParsing ", join(args, " ")
+  let ret2 = parse(all_params, args)
+  echo($ret2)
+  assert ret2.options["-a"].strVal == "-wo"
   assert (not ret2.options.hasKey("test"))
   assert "test" in ret2.files
+
+  # Integer tests.
+  test_success(parse(all_params, @["int test", "-i", "445"]))
+  test_success(parse(all_params, @["int test", "-i", "-445"]))
+  test_failure(EInvalidValue, parse(all_params, @["-i", "fail"]))
+  test_failure(EInvalidValue, parse(all_params, @["-i", "234.12"]))
+  test_failure(EOverflow, parse(all_params, @["-i", $high(int) & "0"]))
+
+  # String tests.
+  test_success(parse(all_params, @["str test", "-a", "word"]))
+  test_failure(EInvalidValue, parse(all_params, @["str test", "-a"]))
+
+  # Float tests.
+  test_success(parse(all_params, @["-f", "123.235"]))
+  test_failure(EInvalidValue, parse(all_params, @["-f", "abracadabra"]))
+  test_failure(EInvalidValue, parse(all_params, @["-f", "12.34aadd"]))
+
+  # Boolean tests.
+  for param in @["y", "yes", "true", "1", "on", "n", "no", "false", "0", "off"]:
+    test_success(parse(all_params, @["-b", param]))
+  test_failure(EInvalidValue, parse(all_params, @["-b", "t"]))
+
+  # Big integer tests.
+  test_success(parse(all_params, @["int test", "-I", "445"]))
+  test_failure(EInvalidValue, parse(all_params, @["-I", "fail"]))
+  test_failure(EInvalidValue, parse(all_params, @["-I", "234.12"]))
+  test_failure(EOverflow, parse(all_params, @["-I", $high(biggestInt) & "0"]))
+
+  # Big float tests.
+  test_success(parse(all_params, @["-F", "123.235"]))
+  test_failure(EInvalidValue, parse(all_params, @["-F", "abracadabra"]))
+  test_failure(EInvalidValue, parse(all_params, @["-F", "12.34aadd"]))
+
+  echo "Tester finished"
+
+when isMainModule:
+  test()
