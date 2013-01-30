fa6dfc42a9456957154e36fdaaaf53158c2dbd2f
Grzegorz Adam Hankiewicz <gradha@imap.cc>
2013-01-30 12:43:30 +0100

    Fixes casing of enum keywords and parsed parameter variables.

--- argument_parser.nim
+++ argument_parser.nim
@@ -4,8 +4,8 @@ import os, strutils, tables, math, parseutils
 
 type
   ## Different types of results for parameter parsing.
-  Tparam_kind* = enum pkEmpty, pkInt, pkFloat, pkString, pkBool,
-    pkBiggestInt, pkBiggestFloat
+  Tparam_kind* = enum PK_EMPTY, PK_INT, PK_FLOAT, PK_STRING, PK_BOOL,
+    PK_BIGGEST_INT, PK_BIGGEST_FLOAT
 
   Tparameter_specification* = object
     ## Holds the expectations of a parameter.
@@ -15,13 +15,13 @@ type
 
   Tparsed_parameter* = object
     case kind*: Tparam_kind
-    of pkEmpty: nil
-    of pkInt: intVal*: int
-    of pkBiggestInt: bigIntVal*: biggestInt
-    of pkFloat: floatVal*: float
-    of pkBiggestFloat: bigFloatVal: biggestFloat
-    of pkString: strVal*: string
-    of pkBool: boolVal*: bool
+    of PK_EMPTY: nil
+    of PK_INT: int_val*: int
+    of PK_BIGGEST_INT: big_int_val*: biggestInt
+    of PK_FLOAT: float_val*: float
+    of PK_BIGGEST_FLOAT: big_float_val: biggestFloat
+    of PK_STRING: str_val*: string
+    of PK_BOOL: bool_val*: bool
 
   Tcommandline_results* = object
     ## Contains the results of the parsing.
@@ -32,14 +32,14 @@ type
 # - Tparameter_specification procs
 
 proc init*(param: var Tparameter_specification,
-    single_word = "", double_word = "", consumes = pkEmpty) =
+    single_word = "", double_word = "", consumes = PK_EMPTY) =
   # Initialization helper.
   param.single_word = single_word
   param.double_word = double_word
   param.consumes = consumes
 
 proc new_parameter_specification*(single_word = "",
-    double_word = "", consumes = pkEmpty): Tparameter_specification =
+    double_word = "", consumes = PK_EMPTY): Tparameter_specification =
   # Initialization helper for let variables.
   result.init(single_word, double_word, consumes)
 
@@ -48,13 +48,13 @@ proc new_parameter_specification*(single_word = "",
 proc `$`*(data: Tparsed_parameter): string =
   # Stringifies the value.
   case data.kind:
-  of pkEmpty: result = "nil"
-  of pkInt: result = $data.intVal
-  of pkBiggestInt: result = $data.bigIntVal
-  of pkFloat: result = $data.floatVal
-  of pkBiggestFloat: result = $data.bigFloatVal
-  of pkString: result = $data.strVal
-  of pkBool: result = $data.boolVal
+  of PK_EMPTY: result = "nil"
+  of PK_INT: result = $data.int_val
+  of PK_BIGGEST_INT: result = $data.big_int_val
+  of PK_FLOAT: result = $data.float_val
+  of PK_BIGGEST_FLOAT: result = $data.big_float_val
+  of PK_STRING: result = $data.str_val
+  of PK_BOOL: result = $data.bool_val
 
 # - Tcommandline_results procs
 
@@ -85,8 +85,8 @@ proc parse_parameter(param, value: string,
   ## will raise (EInvalidValue, EOverflow) if something can't be parsed.
   result.kind = param_kind
   case param_kind:
-  of pkInt:
-    try: result.intVal = value.parseInt
+  of PK_INT:
+    try: result.int_val = value.parseInt
     except EOverflow:
       raise newException(EOverflow, ("parameter $1 requires an " &
         "integer, but $2 is too large to fit into one") % [escape(param),
@@ -95,24 +95,24 @@ proc parse_parameter(param, value: string,
       raise newException(EInvalidValue, ("parameter $1 requires an " &
         "integer, but $2 can't be parsed into one") % [escape(param),
         escape(value)])
-  of pkString:
-    result.strVal = value
-  of pkFloat:
-    try: result.floatVal = value.parseFloat
+  of PK_STRING:
+    result.str_val = value
+  of PK_FLOAT:
+    try: result.float_val = value.parseFloat
     except EInvalidValue:
       raise newException(EInvalidValue, ("parameter $1 requires a " &
         "float, but $2 can't be parsed into one") % [escape(param),
         escape(value)])
-  of pkBool:
-    try: result.boolVal = value.parseBool
+  of PK_BOOL:
+    try: result.bool_val = value.parseBool
     except EInvalidValue:
       raise newException(EInvalidValue, ("parameter $1 requires a " &
         "boolean, but $2 can't be parsed into one. Valid values are: " &
         "y, yes, true, 1, on, n, no, false, 0, off") % [escape(param),
         escape(value)])
-  of pkBiggestInt:
+  of PK_BIGGEST_INT:
     try:
-      let parsed_len = parseBiggestInt(value, result.bigIntVal)
+      let parsed_len = parseBiggestInt(value, result.big_int_val)
       if value.len != parsed_len or parsed_len < 1:
         raise newException(EInvalidValue, ("parameter $1 requires an " &
           "integer, but $2 can't be parsed completely into one") % [
@@ -121,9 +121,9 @@ proc parse_parameter(param, value: string,
       raise newException(EInvalidValue, ("parameter $1 requires an " &
         "integer, but $2 can't be parsed into one") % [escape(param),
         escape(value)])
-  of pkBiggestFloat:
+  of PK_BIGGEST_FLOAT:
     try:
-      let parsed_len = parseBiggestFloat(value, result.bigFloatVal)
+      let parsed_len = parseBiggestFloat(value, result.big_float_val)
       if value.len != parsed_len or parsed_len < 1:
         raise newException(EInvalidValue, ("parameter $1 requires a " &
           "float, but $2 can't be parsed completely into one") % [
@@ -132,7 +132,7 @@ proc parse_parameter(param, value: string,
       raise newException(EInvalidValue, ("parameter $1 requires a " &
         "float, but $2 can't be parsed into one") % [escape(param),
         escape(value)])
-  of pkEmpty:
+  of PK_EMPTY:
     nil
 
 proc parse*(expected: seq[Tparameter_specification],
@@ -177,7 +177,7 @@ proc parse*(expected: seq[Tparameter_specification],
       if lookup.hasKey(arg):
         var parsed : Tparsed_parameter
         let param = lookup[arg]
-        if param.consumes != pkEmpty:
+        if param.consumes != PK_EMPTY:
           if i + 1 < args.len:
             parsed = parse_parameter(arg, args[i + 1], param.consumes)
             i += 1
